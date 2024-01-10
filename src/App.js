fa73e2ab3852eb94ebe4689ade74c08456840370
mbdesigns1989 <mbdesigns1989@yahoo.com>
2024-01-10 06:22:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './components/HomeScreen';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import LoginScreen from './components/LoginScreen';
-
+ 
 function App() {
-  const user = null
   return (
     <div className="app">
-      
-      <Router>
-{!user ? (
-  <LoginScreen/>
-):(
-
-        <Switch>
-          <Route exact path="/">
-      <HomeScreen/>
-
-          </Route>
-        </Switch>
-)}
-      </Router>
+      <h1>Lets build Netflix</h1>
     </div>
   );
 }
