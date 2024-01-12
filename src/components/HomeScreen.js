e9cbe07c94ba1d35504780954c227892811c693a
mbdesigns1989 <mbdesigns1989@yahoo.com>
2024-01-12 13:26:19 +0200

    Update nav component

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,1 +1,14 @@
- 
+import React from 'react';
+import "./HomeScreen.css";
+import Nav from './Nav'
+
+
+
+const HomeScreen = () => {
+    return <div>
+        <Nav/>
+        {/* banner */}
+    </div>;
+}
+
+export default HomeScreen;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,1 +1,21 @@
- 
+import React from 'react';
+import "./Nav.css";
+
+const Nav = () => {
+    return <div className="nav">
+        <div className="nav__contents">
+            <img
+                className="nav__logo"
+                src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                alt="" />
+            <img
+                className="nav__avatar"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
+                alt="" />
+
+        </div>
+
+    </div>;
+}
+
+export default Nav;
