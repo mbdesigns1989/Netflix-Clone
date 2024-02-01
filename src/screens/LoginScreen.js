da5ba5d6765df4e7be94a070de363943ac3b7d4a
mbdesigns1989 <mbdesigns1989@yahoo.com>
2024-02-01 02:42:25 +0200

    Add signupscreen component

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './LoginScreen.css';
-
-
+import { useState } from 'react';
+import SignupScreen from "./SignupScreen";
 
 
 const LoginScreen = () => {
+    const [signIn, setSignIn] = useState(false)
+
     return (<div className="loginScreen">
         <div className="loginScreen__background">
             <img className="loginScreen__logo"
@@ -16,28 +18,31 @@ const LoginScreen = () => {
                 </button>
 
             <div className="loginScreen__gradient" />
-
         </div>
-
         <div className="loginScreen__body">
+            {signIn ? (
+                <SignupScreen />
+            ) : (
 
-            <>
-                <h1>Unlimited films, TV programmes and more.</h1>
-                <h2>Watch anywhere. Cancel at anytime</h2>
-                <h3>Ready to watch? Enter your email to create or restart your membership</h3>
-                <div className="loginScreen__input">
-                    <form >
-                        <input type="email" placeholder="Email address" />
-                        <button className="loginScreen__getStarted">GET STARTED</button> 
-                    </form>
-                </div>
-            </>
-
+                <>
+                    <h1>Unlimited films, TV programmes and more.</h1>
+                    <h2>Watch anywhere. Cancel at anytime</h2>
+                    <h3>Ready to watch? Enter your email to create or restart your membership</h3>
+                    <div className="loginScreen__input">
+                        <form >
+                            <input type="email" placeholder="Email address" />
+                            <button className="loginScreen__getStarted" onClick={(e) => {
+                                e.preventDefault()
+                                setSignIn(true)
+                            }}>GET STARTED</button>
+                        </form>
+                    </div>
+                </>
+            )}
         </div>
-
-    </div>);
+    </div>
+    )
 }
-
 
 
 export default LoginScreen;
