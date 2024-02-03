87f48e8912430c3ed4f995e5141da489682f2109
mbdesigns1989 <mbdesigns1989@yahoo.com>
2024-02-03 18:42:53 +0200

    Update firebase authentication

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import './SignupScreen.css';
-import { useRef } from 'react';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import app from '../firebase';
-
 
 
 const SignupScreen = () => {
 
-    const emailRef = useRef(null);
-    const passwordRef = useRef(null);
-
-
     const register = (e) => {
         e.preventDefault();
 
-        const auth = getAuth(app);
-
-        createUserWithEmailAndPassword(
-            emailRef.current.value,
-            passwordRef.current.value,
-            auth
-        ).then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user)
-
-        }).catch((error) => {
-
-            alert(error.message);
-
-        })
     };
-
 
 
     const signIn = (e) => {
@@ -42,8 +18,8 @@ const SignupScreen = () => {
     return (<div className="signupScreen">
         <form>
             <h1>Sign In</h1>
-            <input ref={emailRef} type="email" placeholder="Email address" />
-            <input ref={passwordRef} type="password" placeholder="Password" />
+            <input type="email" placeholder="Email address" />
+            <input type="password" placeholder="Password" />
             <button type="submit" onClick={signIn}>Sign In</button>
             <h4>
                 <span className="signupScreen__gray"> New to Netflix? </span>
